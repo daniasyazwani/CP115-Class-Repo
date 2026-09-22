d0c70873080f4cd7befe95712158abb028cb0640
daniasyazwani <nurdaniasyazwani1186@gmail.com>
2026-09-22 10:31:11 +0800

    done

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -1,6 +1,13 @@
 num_rounds = int(input())
+#initialize
+rounds_processed = num_rounds
+final_score = 0
 
-
+for i in range (num_rounds):
+    score = float(input())
+    if score > 100:
+        score += score * 0.2
+    final_score += score
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
